430d02d64e11f87f7ac1a43baf218a7d0bdaca42
bottassi <bottassi2012@gmail.com>
2023-02-01 12:59:06 -0300

    validaciones mail, name,lastname

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :lastname).except(:password_confirmation)
+  end
   def index; end
 
   def new
@@ -6,7 +9,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params[:user])
+    
+    
+    if user_params["password"] == params["user"]["password_confirmation"]
+      @user = User.new(user_params)
+      if @user.save
+        flash[:notice] = "Successfully created user."
+      else
+        flash[:error] = "Error creating user."
+      end
+    else
+      flash[:error] = "Passwords don't match."
+    end
+    @user
   end
 
   def log_in; end
