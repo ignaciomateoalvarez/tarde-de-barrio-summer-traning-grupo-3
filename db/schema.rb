6148c86063b5b5cea73dfd903af3fc48e5c7d7a3
lautaromastronardi <lautaromastronardi@gmail.com>
2023-02-08 13:08:07 -0300

    switch de estado funcionando

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_07_133514) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_08_132803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_07_133514) do
     t.string "remember_me_token"
     t.datetime "remember_me_token_expires_at"
     t.integer "rol", default: 0
+    t.boolean "active", default: true
     t.index ["remember_me_token"], name: "index_users_on_remember_me_token"
   end
 
