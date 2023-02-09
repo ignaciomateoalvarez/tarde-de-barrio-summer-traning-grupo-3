b1485b1912ba60d4ac8949a19ea41f93988022c3
bottassi <bottassi2012@gmail.com>
2023-02-09 15:41:45 -0300

    rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
 
   describe 'Validations' do
-    subject { build(:user) }
-    it { should validate_presence_of(:name) }
-    it { should validate_uniqueness_of(:email) }
-    #it { is_expected.to validate_presence_of(:email) }
-    #it { is_expected.to validate_presence_of(:name) }
-    #it { is_expected.to validate_presence_of(:lastname) }
-    #it { is_expected.to validate_length_of(:password).is_at_least(8) }
+    subject { create(:user) }
+    it do
+       is_expected.to validate_presence_of(:name)
+     end
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:lastname) }
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
   end
 
   describe 'Factory' do
